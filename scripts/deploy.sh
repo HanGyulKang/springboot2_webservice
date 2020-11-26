820985234769ecb7e89988eec3531126f42f1f5a
kanghangyul <gang6607@gmail.com>
2020-11-26 21:50:54 +0900

    2020 11 26 21:50 commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot2_webservice | grep jar | awk '{print $1')
+CURRENT_PID=$(pgrep -fl springboot2_webservice | grep jar | awk '{print $1}')
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
